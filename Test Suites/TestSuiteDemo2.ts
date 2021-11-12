3905d1c0d584042cb19dd9b7c9d9dfcb491d9339
Binh Tran <btbinhtran@gmail.com>
2021-11-12 09:43:57 -0800

    Added TestSuiteDemo2

---
+++ Test Suites/TestSuiteDemo2.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuiteDemo2</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>f3d303da-15dc-4802-87d7-1e1a8b0c9350</testSuiteGuid>
+   <testCaseLink>
+      <guid>f7acacd1-4db9-4bcc-af44-be6a931aeaa3</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Demo/RecordModeDemo</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
